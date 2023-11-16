d9cf78c11912bb6b2f9fdf8bcc04449dca1a0abb
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-11-15 16:07:45 -0800

    added new elements

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,26 @@
 import '@styles/globals.css';
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts"
 }
 
-export default layout
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav/>
+          {children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout
