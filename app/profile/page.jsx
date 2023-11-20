f985516672090b6ad4f4f88bd6d855f9b521c057
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-11-19 20:18:45 -0800

    add endpoints for prompt

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,6 +8,7 @@ import Profile from '@components/Profile';
 
 
 const MyProfile = () => {
+  const router = useRouter();
     const { data: session } = useSession();
 
     const [posts, setPosts] = useState([]);
@@ -21,8 +22,8 @@ const MyProfile = () => {
         if (session?.user.id) fetchPosts();
       }, []);
 
-    const handleEdit = () => {
-        
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post.id}`)
     }
 
     const handleDelete = async () => {
